76d141de808c085b9fec1b67468f895990c6abcc
bakare muideen adeleke <bakare1234@gmail.com>
2022-07-05 09:36:41 -0700

    Task 0 completed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,17 @@
  */
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
 }
+
 

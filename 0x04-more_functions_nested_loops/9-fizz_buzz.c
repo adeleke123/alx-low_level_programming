ff9fb76aa772f03aba5005a1b79f6a6c0057d317
bakare muideen adeleke <bakare1234@gmail.com>
2022-07-07 11:02:17 -0700

    Task 9 completed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - print fizzbuzz
+ * main - Fizz buszz challenge
+ *
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
+		if (i % 15 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 3 == 0)
 		{
-			printf("Buzz");
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		if (i == 100)
-		{
-			continue;
-		}
-		printf(" ");
 	}
 	printf("\n");
 

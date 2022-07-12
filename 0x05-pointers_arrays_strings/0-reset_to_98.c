cb695be3317564383d081e40d42e6e1eed6f0dfe
bakare muideen adeleke <bakare1234@gmail.com>
2022-07-12 02:24:17 -0700

    Task completed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the value of the memory location it accepts to 98
+ * @n: This is a pointer
+ *
+ * Return: Null Void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

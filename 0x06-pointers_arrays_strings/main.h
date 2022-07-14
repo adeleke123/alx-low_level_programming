e96376c9736cc1039f749126a64d3f6b9d7e365a
bakare muideen adeleke <bakare1234@gmail.com>
2022-07-14 15:22:28 -0700

    Task completed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,3 @@
-#ifndef main_H
-#define main_H
-
-/*adeleke*/
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -17,5 +12,3 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int main(void);
-
-#endif /*adeleke*/

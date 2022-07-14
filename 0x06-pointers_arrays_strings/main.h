66245a3a81a1def1ad420e6e36bffd8aba0411d0
bakare muideen adeleke <bakare1234@gmail.com>
2022-07-14 12:40:54 -0700

    Task completed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#include <unistd.h>
+#ifndef main_H
+#define main_H
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,4 +15,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int main(void);
+
+#endif

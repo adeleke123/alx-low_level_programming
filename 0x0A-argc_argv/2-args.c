80463a74039d4f55380534243877898f0807be04
bakare muideen adeleke <bakare1234@gmail.com>
2022-07-22 09:24:51 -0700

    Task completed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,10 @@
  *
  * Return: 0
  */
-int main(int argc, char **argv[])
-{
-	int x;
 
-	for (x = 0; x < argc; x++)
-		printf("%s\n", argv[x]);
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }

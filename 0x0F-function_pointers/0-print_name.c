e669b02fcc3188f14624329b500584ac49e41c5f
bakare muideen adeleke <bakare1234@gmail.com>
2022-08-03 06:37:42 -0700

    Task completed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,13 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: pointer to a function that prints a name
+ *
+ * Return: Null Void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		retutn;
+		return;
 
 	f(name);
 }

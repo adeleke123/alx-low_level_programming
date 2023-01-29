aa689ac303accd82a65c38301e540a99118fd1da
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-30 00:19:48 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
 
-    if (!array == NULL)
+    if (!array || size == NULL)
         return (-1);
 
     for (i = 0; i < size; i++)

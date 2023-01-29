be18b8bfeeaf5c111603e4928251b933eed9bde5
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-30 00:17:34 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,14 +17,14 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
 
-    if (array == NULL)
+    if (!array == NULL)
         return (-1);
 
     for (i = 0; i < size; i++)
     {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
         if (array[i] == value)
-            return (i);
+            return (int)i;
     }
 
     return (-1);
